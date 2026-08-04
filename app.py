336517ea846962c2ccb9ffcf3d606896d33307bb
studylisa10-blip <studylisa10@gmail.com>
2026-08-04 10:45:31 +0100

    requirements.txt
    streamlit

---
+++ app.py
@@ -0,0 +1,21 @@
+impo** stream**t as st
+
+st.set**age_config(page_title="**sa's Daily Dashboard")
+
+st.title**☕ Lisa's Daily Dashboard")
+
+st.he**er**⚽ Football")
+
+st.write**Manchester United fixtures will g**here")
+
+st.header**📰 News")
+
+st.write("Personalised**ews will go here")
+
+st.header**⚡ Smart Metering")
+
+st.write("Ind**try updates will go here")
+
+st.he**er("📚 Learning")
+
+st.write**Databricks,**QL and Power BI updates")
