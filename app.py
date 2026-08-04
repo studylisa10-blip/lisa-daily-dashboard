e5e5d40b48d088801480875effd3832ed36ad618
studylisa10-blip <studylisa10@gmail.com>
2026-08-04 12:48:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def get_weather():
 # FIXTURES
 # --------------------------------------------------
 
-mens_fixtures = [     {         "home": "Manchester United",         "away": "Paris Saint-Germain",         "date": "08 Aug 2026",         "time": "17:00",= [
+mens_fixtures = [     {         {     "home": "Manchester United",     "away": "Chelsea Women",     "date": "13 Sep 2026",     "time": "13:00",         "away": "Paris Saint-Germain",         "date": "08 Aug 2026",         "time": "17:00",= [
     {
         "home": "Manchester United",
         "away": "Paris Saint-Germain",
